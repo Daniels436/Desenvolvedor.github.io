7ec538a594148babb1e96aedbceea79193a085a9
Daniel Souza <69437942+Daniels436@users.noreply.github.com>
2024-02-17 11:28:24 -0300

    Adiciona estilo CSS e atualizações de texto para botão e balão informativo
    Implementação de melhorias de usabilidade no botão de voltar ao topo e no balão informativo, tornando a interação mais intuitiva e agradável para os usuários.

--- JS/slid-cel.js
+++ JS/slid-cel.js
@@ -19,3 +19,37 @@
     var menuContent = document.querySelector(".menu-content");
     menuContent.style.display = (menuContent.style.display === "block") ? "none" : "block";
   }
+
+
+
+
+
+
+
+//BALAO-INFORMATIVO-DE-TOPO-DA-PAGINA
+var btnVoltarTopo = document.querySelector('.btn-topo');
+var balaoInformativo = document.getElementById('balao-informativo');
+
+btnVoltarTopo.addEventListener('click', function(event) {
+  event.preventDefault(); // Impede o comportamento padrão do link
+
+  // Verifica se já está no topo da página
+  if (window.pageYOffset === 0) {
+    exibirBalaoInformativo();
+  } else {
+    voltarAoTopo();
+  }
+});
+
+function voltarAoTopo() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+
+function exibirBalaoInformativo() {
+  balaoInformativo.style.display = 'block';
+
+  // Remove o balão informativo após 2 segundos
+  setTimeout(function() {
+    balaoInformativo.style.display = 'none';
+  }, 2000);
+}
